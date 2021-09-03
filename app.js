965646d25bb83d52f9577c414db4f9c73143ac42
panagiwthsv <panagiwthsv@hotmail.com>
2021-09-03 12:10:14 +0300

    adding files

---
+++ app.js
@@ -0,0 +1,24 @@
+const countdown = () => {
+    var launch = new Date("September 5, 2021 21:00:00")
+
+    var now = new Date().getTime();
+    var timeleft = launch - now;
+    
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+    days = days < 10 ? "0" + days : days;
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+}
+
+setInterval(countdown,1000);
